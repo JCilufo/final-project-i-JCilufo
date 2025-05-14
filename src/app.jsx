8a2366e312f1d6de8457f380edddff27be639098
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-05-14 09:05:11 -0500

    feat: notify api to return json data

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,16 @@
 import Header from './components/header'
 import StarshipGrid from './components/starship-grid/index'
 import { useState, useEffect } from 'react'
+
+const options = {
+  method: 'GET',
+
+  // `headers` notify the API that we want JSON data
+  headers: {
+    accept: 'application/json',
+  },
+}
+
 export default function App() {
   return (
     <main>
